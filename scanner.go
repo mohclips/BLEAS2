3e42722c3f7cdea41c1827fab0388866c9d9c48a
mohclips <mohclips@gmail.com>
2021-01-09 22:17:37 +0000

    add check root/sudo and fix sb power issue

--- scanner.go
+++ scanner.go
@@ -8,12 +8,19 @@ import (
 	"encoding/hex"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/pkg/errors"
 	"github.com/sausheong/ble"
 	"github.com/sausheong/ble/examples/lib/dev"
+	// "github.com/gernest/wow"
 )
 
 var (
@@ -24,8 +31,21 @@ var (
 
 var isRunning bool = false
 
+const debug bool = false
+
 func main() {
+
+	// needed to access Bluetooth and set USB power
+	if !runningAsRoot() {
+		log.Fatal("This program must be run as root! (sudo)")
+	}
+
+	// make sure USB power is 'on' not 'auto' otherwise we have s suspend issue
+	checkBtUsbPower()
+
 	flag.Parse()
+
+	fmt.Printf("Running...\n")
 
 	d, err := dev.NewDevice(*device)
 	if err != nil {
@@ -33,14 +53,19 @@ func main() {
 	}
 	ble.SetDefaultDevice(d)
 
+	// best struct pprint
+	//spew.Dump(d)
+
 	isRunning = true
 
-	// Scan for specified durantion, AND until interrupted by user.
+	// Scan for specified duration, AND until interrupted by user.
 	for isRunning {
 		ctx := ble.WithSigHandler(context.WithTimeout(context.Background(), *du))
 		chkErr(ble.Scan(ctx, *dup, advHandler, nil))
 	}
 }
+
+// #######################################################################################
 
 func advHandler(a ble.Advertisement) {
 
@@ -119,3 +144,125 @@ func formatHex(instr string) (outstr string) {
 	}
 	return
 }
+
+// #######################################################################################
+
+func runningAsRoot() bool {
+	// https://www.socketloop.com/tutorials/golang-force-your-program-to-run-with-root-permissions
+	cmd := exec.Command("id", "-u")
+	output, err := cmd.Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// output has trailing \n
+	// need to remove the \n
+	// otherwise it will cause error for strconv.Atoi
+	// log.Println(output[:len(output)-1])
+
+	// 0 = root, 501 = non-root user
+	i, err := strconv.Atoi(string(output[:len(output)-1]))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if i != 0 {
+		return false
+	}
+
+	return true
+}
+
+// #######################################################################################
+
+// Exists reports whether the named file or directory exists.
+// https://stackoverflow.com/a/12527546/7396553
+func Exists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+func setUsbDevPower(p string, power string) {
+	fmt.Printf("Setting %s to %q\n", p, power)
+
+	data := []byte(power + "\n")
+	err := ioutil.WriteFile(p, data, 644)
+	if err != nil {
+		panic(err)
+	}
+}
+func findDevPower(p string) {
+
+	splitPath := strings.Split(p, "/")
+
+	//fmt.Printf("%s\n", splitPath)
+
+	// path position to start in
+	var hubStart bool = false
+	var startPos int = 0
+	for i := range splitPath {
+		if strings.Contains(splitPath[i], "usb") {
+			hubStart = true
+			startPos = i + 1
+		}
+		if hubStart && (i > startPos) {
+			currentPath := strings.Join(splitPath[:i], "/")
+			controlFile := currentPath + "/power/control"
+			if Exists(controlFile) {
+				if debug {
+					fmt.Printf("found: %s\n", controlFile)
+				}
+
+				data, _ := ioutil.ReadFile(controlFile)
+				//fmt.Print(string(data))
+
+				controlValue := strings.TrimSpace(string(data))
+				if controlValue != "on" {
+					if debug {
+						fmt.Printf("Warning: USB device not set correctly [Power=%s]\n", controlValue)
+					}
+					setUsbDevPower(controlFile, "on")
+				}
+
+			}
+		}
+	}
+
+}
+
+func checkBtUsbPower() {
+	const bluetoothPath = "/sys/class/bluetooth/"
+
+	// get all hci device names
+	files, err := ioutil.ReadDir(bluetoothPath)
+	if err != nil {
+		log.Fatal("No HCI devices found, odd: ", err)
+	}
+
+	// get paths to each device
+	for _, f := range files {
+		//fmt.Println(f.Name())
+		btDev, _ := os.Readlink(bluetoothPath + f.Name())
+		//fmt.Println(btDev)
+
+		absBtDev, _ := filepath.Abs(bluetoothPath + btDev)
+		//fmt.Println(absBtDev)
+
+		if Exists(absBtDev) {
+			if debug {
+				fmt.Printf("Device path exists: %s\n", absBtDev)
+			}
+
+			findDevPower(absBtDev)
+		}
+
+	}
+}
+
+// #######################################################################################
